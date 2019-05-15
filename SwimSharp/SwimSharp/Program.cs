7ac07cbab4aaff884b732b998054adb19afc47d4
sacha barber <sbarber@nephilaadvisors.co.uk>
2019-05-15 19:57:00 +0100

    scaffolding

---
+++ SwimSharp/SwimSharp/Program.cs
@@ -0,0 +1,59 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using Akka.DI.Core;
+using Autofac;
+using Serilog;
+using System;
+
+namespace SwimSharp
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                 .WriteTo.Console()
+                 .MinimumLevel.Information()
+                 .CreateLogger();
+
+            Serilog.Log.Logger = logger;
+
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterInstance<ILogger>(Serilog.Log.Logger);
+            builder.RegisterType<MyActor>();
+            IContainer container = builder.Build(); 
+
+
+            var system = ActorSystem.Create("my-test-system", "akka { loglevel=INFO,  loggers=[\"Akka.Logger.Serilog.SerilogLogger, Akka.Logger.Serilog\"]}");
+            system.UseAutofac(container);
+
+
+            //var actor = system.ActorOf(MyActor.Props(), $"demo-{Guid.NewGuid().ToString("N")}");
+            var actor = system.ActorOf(system.DI().Props<MyActor>(), $"demo-{Guid.NewGuid().ToString("N")}");
+
+            actor.Tell("dddsdsadsada");
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MyActor : ReceiveActor
+    {
+
+        public MyActor(ILogger logger)
+        {
+            Receive<string>(message => {
+                logger.Information($"Got message {message}");
+            });
+        }
+
+        public static Props Props(ILogger logger)
+        {
+            return Akka.Actor.Props.Create(() => new MyActor(logger));
+        }
+    }
+
+}
