214f36b9697bec8deb86faee2f5822f9553bebae
sacha barber <sbarber@nephilaadvisors.co.uk>
2019-05-16 21:10:20 +0100

    adding web api with autofac

---
+++ SwimSharp/WebApiSelfHosted/WebApiSelfHosted/Writer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiSelfHosted
+{
+    public interface IWriter
+    {
+        void Write();
+    }
+
+    public class FooWriter : IWriter
+    {
+        public void Write()
+        {
+
+        }
+    }
+}
